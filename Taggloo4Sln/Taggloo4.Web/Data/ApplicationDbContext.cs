45989dd7586882abb724e2cdbd7a78bc1f46f4ab
Nathan Pledger <nathan@programx.co.uk>
2023-09-30 18:03:13 +0100

    Fixing Rider's Git commit

---
+++ Taggloo4Sln/Taggloo4.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using Taggloo4.Web.Models;
+
+namespace Taggloo4.Web.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+{
+	public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+		: base(options, operationalStoreOptions)
+	{
+	}
+}
