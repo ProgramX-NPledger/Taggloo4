c764b3e9aa639cb4fd21e622c6a8eb11f305f827
Nathan Pledger <nathan@programx.co.uk>
2024-01-07 14:10:39 +0000

    Improving Swagger documentation

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,7 @@
+using System.Reflection;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +15,20 @@ builder.Services.AddDbContext<DataContext>(opt =>
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    // options.SwaggerDoc("v4",new OpenApiInfo()
+    // {
+    //     Version = "v4",
+    //     Title = "Taggloo API",
+    //     Description = "API for autonomous interaction with Taggloo data",
+    //     // TODO: set details
+    //     
+    // });
+    
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
