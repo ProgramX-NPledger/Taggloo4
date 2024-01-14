6425bf0cfc6ce8d9e81cf140b617bcdb783f5912
Nathan Pledger <nathan@programx.co.uk>
2024-01-14 14:32:54 +0000

    Adding migration not added to Git in last commit

---
+++ API/Data/Migrations/20240114140203_AddDictionaries.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDictionaries : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Dictionaries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 1024, nullable: false),
+                    SourceUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    IetfLanguageTag = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedByUserName = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    CreatedOn = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dictionaries", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Dictionaries_Languages_IetfLanguageTag",
+                        column: x => x.IetfLanguageTag,
+                        principalTable: "Languages",
+                        principalColumn: "IetfLanguageTag",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dictionaries_IetfLanguageTag",
+                table: "Dictionaries",
+                column: "IetfLanguageTag");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dictionaries");
+        }
+    }
+}
