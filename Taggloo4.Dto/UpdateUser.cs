a673538a2dfea1102cf46b0304eb2cbf3fc91641
ProgramX-NPledger <nathan@programx.co.uk>
2024-02-19 22:49:55 +0000

    Implemented modification of User, including changing of password, roles modification

---
+++ Taggloo4.Dto/UpdateUser.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Taggloo4.Dto;
+
+/// <summary>
+/// Represents a User to be updated using the PATCH Users method.
+/// </summary>
+public class UpdateUser
+{
+
+	/// <summary>
+	/// Username of the User to update.
+	/// </summary>
+	public required string UserName { get; set; }
+
+	/// <summary>
+	/// Role names the User should be a Member of.
+	/// </summary>
+	public string[]? Roles { get; set; }
+
+	/// <summary>
+	/// If set, changes the User's password.
+	/// </summary>
+	public ChangePassword? ChangePassword { get; set; }
+	
+	
+	
+}
