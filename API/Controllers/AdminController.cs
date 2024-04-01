a1ba2923e6199d3275a1fbced6aa1d08725590c9
ProgramX-NPledger <nathan@programx.co.uk>
2024-04-01 12:48:38 +0100

    Authenticationf or JWT and Cookies working.

---
+++ API/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+//[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+public class AdminController : Controller
+{
+    [Authorize(Roles = "administrator")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    
+}
