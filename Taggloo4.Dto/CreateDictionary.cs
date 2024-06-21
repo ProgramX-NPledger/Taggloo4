527121f2ddd22202142ecf2f2d274890d0ddd294
ProgramX-NPledger <nathan@programx.co.uk>
2024-06-21 14:41:20 +0100

    Reinforced NULLability constraints, reduced permitted string widths

--- Taggloo4.Dto/CreateDictionary.cs
+++ Taggloo4.Dto/CreateDictionary.cs
@@ -10,39 +10,43 @@ public class CreateDictionary
 	/// <summary>
 	/// Name of the Dictionary.
 	/// </summary>
+	[MaxLength(128)]
 	public required string Name { get; set; }
 
 	/// <summary>
 	/// Description of the Dictionary.
 	/// </summary>
+	[MaxLength(1024)]
 	public required string Description { get; set; }
 
 	/// <summary>
 	/// URL of the source of the Dictionary.
 	/// </summary>
+	[MaxLength(1024)]
 	public required string SourceUrl { get; set; }
 
 	/// <summary>
 	/// IETF Language Tag for the Dictionary.
 	/// </summary>
+	[MaxLength(5)]
 	public required string IetfLanguageTag { get; set; }
 	
 	/// <summary>
 	/// The URL name of the Controller to use to retrieve content.
 	/// </summary>
 	[MaxLength(32)]
-	public string? Controller { get; set; }
+	public required string Controller { get; set; }
 
 	/// <summary>
 	/// Disambiguated identifier for type of content to allow automatic processing.
 	/// </summary>
 	[MaxLength(32)]
-	public string? ContentTypeKey { get; set; }
+	public required string ContentTypeKey { get; set; }
 
 	/// <summary>
 	/// Human-suitable name of Content Type.
 	/// </summary>
 	[MaxLength(128)]
-	public string? ContentTypeFriendlyName { get; set; }
+	public required string ContentTypeFriendlyName { get; set; }
 	
 }
