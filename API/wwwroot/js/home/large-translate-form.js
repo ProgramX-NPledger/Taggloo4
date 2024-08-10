3889a015d98334cfda34d6f3cfe821ea8749d670
ProgramX-NPledger <nathan@programx.co.uk>
2024-08-10 16:16:59 +0100

    Implementation of flipping of languages

---
+++ API/wwwroot/js/home/large-translate-form.js
@@ -0,0 +1,22 @@
+﻿var fromLanguageCode='';
+var toLanguageCode = '';
+
+function captureLanguageCodes() {
+    const fromLanguageCodeElement = document.getElementById('FromLanguageCode');
+    const toLanguageCodeElement = document.getElementById('ToLanguageCode');
+    if (fromLanguageCodeElement && toLanguageCodeElement) {
+        fromLanguageCode=fromLanguageCodeElement.value;
+        toLanguageCode=toLanguageCodeElement.value;
+    }
+}
+function flipLanguageCodes() {
+    const fromLanguageCodeElement = document.getElementById('FromLanguageCode');
+    const toLanguageCodeElement = document.getElementById('ToLanguageCode');
+    if (fromLanguageCodeElement && toLanguageCodeElement) {
+        toLanguageCodeElement.value=fromLanguageCode;
+        fromLanguageCodeElement.value=toLanguageCode;
+        let otherLanguageCode = fromLanguageCode;
+        fromLanguageCode=toLanguageCode;
+        toLanguageCode=otherLanguageCode;
+    } 
+}
