d7b22fc5ebcd14f4a044c773e55fdf5165e80dec
ProgramX-NPledger <nathan@programx.co.uk>
2024-09-04 20:02:47 +0100

    Added XML documentation

--- Taggloo4.Web/Translation/DefaultTranslatorConfiguration.cs
+++ Taggloo4.Web/Translation/DefaultTranslatorConfiguration.cs
@@ -2,7 +2,13 @@
 
 namespace Taggloo4.Web.Translation;
 
+/// <summary>
+/// Default configuration applied to a Translator when a defined configuration cannot be resolved.
+/// </summary>
 public class DefaultTranslatorConfiguration : ITranslatorConfiguration
 {
+    /// <summary>
+    /// Whether the Translator is enabled for use. Always <c>True</c>.
+    /// </summary>
     public bool IsEnabled { get; } = true;
 }
