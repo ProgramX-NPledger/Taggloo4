1f5ff1a7a8968c8f5d26dd5ea67221fd91220cdb
ProgramX-NPledger <nathan@programx.co.uk>
2024-09-04 22:50:04 +0100

    Experimenting with sorting

--- Taggloo4.Web/wwwroot/js/translate/translate.js
+++ Taggloo4.Web/wwwroot/js/translate/translate.js
@@ -4,11 +4,23 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/translate").build(
 
 // this is from the server
 connection.on("UpdateTranslationResults", function (translator,priority,result) {
-    console.log(translator,priority);
     const translationResultElementId="translationResults_"+translator;
-    let div = $("<div>", {id: translationResultElementId, "class": "translation-result translation-result-"+translator});
+    let div = $("<div>", {id: translationResultElementId, "class": "translation-result translation-result-"+translator, 'data-priority':priority});
     div.append(result);
 
+    // iterate through elements inside #translationResults
+    // as soon as see a priority > incoming priority, prepend content
+
+    $('#translationResults').children('.translation-result').each(function (ordinal) {
+        //alert(this.value); // "this" is the current element in the loop
+        console.log(translator,ordinal,$(this).attr('data-priority'));
+        const nextPriority=$(this).attr('data-priority');
+        console.log(nextPriority,priority,nextPriority>=priority);
+        if (nextPriority>=priority) { // this isn't working
+            $("#translationResults").prepend(div);
+        }
+    });
+    
     $("#translationResults").append(div);
 });
     
