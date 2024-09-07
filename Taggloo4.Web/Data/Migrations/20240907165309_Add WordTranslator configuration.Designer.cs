bb277ce44837bbc2dbf2c0f91b54526c817436f4
ProgramX-NPledger <nathan@programx.co.uk>
2024-09-07 17:55:04 +0100

    Added WordTranslator seeded configuration

--- Taggloo4.Web/Data/Migrations/20240907165309_Add WordTranslator configuration.Designer.cs
+++ Taggloo4.Web/Data/Migrations/20240907165309_Add WordTranslator configuration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Taggloo4.Web.Data;
 
@@ -11,9 +12,11 @@ using Taggloo4.Web.Data;
 namespace Taggloo4.Web.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240907165309_Add WordTranslator configuration")]
+    partial class AddWordTranslatorconfiguration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
