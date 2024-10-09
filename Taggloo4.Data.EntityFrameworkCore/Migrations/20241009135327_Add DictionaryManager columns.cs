4814cdd2084631580a9ae00301b2a542ef98f29f
ProgramX-NPledger <nathan@programx.co.uk>
2024-10-09 16:32:47 +0100

    Adding missing migrations files

---
+++ Taggloo4.Data.EntityFrameworkCore/Migrations/20241009135327_Add DictionaryManager columns.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Taggloo4.Data.EntityFrameworkCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDictionaryManagercolumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DictionaryManagerFactoryDotNetAssemblyName",
+                table: "Dictionaries",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DictionaryManagerFactoryDotNetTypeName",
+                table: "Dictionaries",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DictionaryManagerFactoryDotNetAssemblyName",
+                table: "Dictionaries");
+
+            migrationBuilder.DropColumn(
+                name: "DictionaryManagerFactoryDotNetTypeName",
+                table: "Dictionaries");
+        }
+    }
+}
